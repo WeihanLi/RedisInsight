82ab61490a15a694112a386afa89457cddb2e7b6
Weihan Li <weihanli@outlook.com>
2024-04-11 21:40:11 +0800

    update release version

---
+++ scripts/export-release-version.cs
@@ -0,0 +1,7 @@
+using var packageJsonFileStream = File.OpenRead("./redisinsight/package.json.json");
+var versionPrefix = System.Text.Json.JsonSerializer.Deserialize<System.Text.Json.Nodes.JsonObject>(packageJsonFileStream)?["sdk"]?["version"]?.GetValue<string>();
+var versionSuffix = DateTime.UtcNow.ToString("yyyyMMdd");
+var version = $"{versionPrefix}-{versionSuffix}".Trim('-');
+var envFile = Environment.GetEnvironmentVariable("GITHUB_ENV");
+File.WriteAllText(envFile, $"ReleaseVersion={version}");
+Console.WriteLine(File.ReadAllText(envFile));
