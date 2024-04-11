1c407d2283654205c25ff98a3d1a594f863e08b2
Weihan Li <weihanli@outlook.com>
2024-04-11 21:42:52 +0800

    fix file path

--- scripts/export-release-version.cs
+++ scripts/export-release-version.cs
@@ -1,4 +1,4 @@
-using var packageJsonFileStream = File.OpenRead("./redisinsight/package.json.json");
+using var packageJsonFileStream = File.OpenRead("./redisinsight/package.json");
 var versionPrefix = System.Text.Json.JsonSerializer.Deserialize<System.Text.Json.Nodes.JsonObject>(packageJsonFileStream)?["version"]?.GetValue<string>();
 var versionSuffix = DateTime.UtcNow.ToString("yyyyMMdd");
 var version = $"{versionPrefix}-{versionSuffix}".Trim('-');
